1f6c28425b0be3879766f9ccd42215793449d671
kaede0902 <kaede0902js@gmail.com>
2019-06-28 20:40:35 +0900

    update

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -47,8 +47,8 @@ class ArticlesController extends AppController
             }
             $this->Flash->error(__('Unable to add your article.'));
         }
-        #$tags = $this->Articles->Tags->find('list');
-        #$this->set('tags', $tags);
+        $tags = $this->Articles->Tags->find('list');
+        $this->set('tags', $tags);
         $this->set('article', $article);
     }
 
--- src/Model/Table/PeopleTable.php
+++ src/Model/Table/PeopleTable.php
@@ -1,7 +1,45 @@
 <?php
 namespace App\Model\Table;
 use Cake\ORM\Table;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Validator;
 
 class PeopleTable extends Table {
+    public function inititalize(array $config) {
+        parent::inititalize($config);
+        $this->setTable('people');
+        $this->setDisplayField('mail');
+        $this->setPrimaryKey('id');
+    }
+    public function findMe(Query $query, array $options) {
+        $me = $options['me'];
+        return $query
+            ->where(['name like' => '%'.$me.'%'])
+            ->orWhere(['mail like' => '%'.$me.'%'])
+            ->order(['age'=>'asc']);
+    }
+    public function findAge(Query $query, array $options) {
+        return $query
+            ->order(['age'=>'asc'])
+            ->order(['name'=>'asc']);
+    }
+    public function validationDefault(Validator $validator) {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id','create');
+        $validator
+            ->scalar('name')
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+        $validator
+            ->scalar('mail')
+            ->allowEmpty('mail');
+        $validator
+            ->scalar('age')
+            ->requirePresence('age', 'create')
+            ->notEmpty('age');
 
+        return $validator;
+    }
 }
